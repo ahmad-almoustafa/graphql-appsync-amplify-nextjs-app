60f49b690f7372386803512d64dd6fb1217bdc92
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-07-18 19:36:30 +0800

    amplify: regenerate the code for API using typescript

--- docs.sh
+++ docs.sh
@@ -10,14 +10,22 @@ npm i @aws-amplify/ui-react aws-amplify
 
 #add a new GraphQL API to your Amplify project.
 #by default he API will be configured to use DynamoDB as the data source. 
+#AppSyncBlogApp
 amplify add api
 
-# deploy your local backend resources defined in the Amplify project to the cloud.
+#deploy your local backend resources defined in the Amplify project to the cloud.
+#Do you want to generate code for your newly created GraphQL API =>
+#this will genearte the 'src/graphql' folder + aws-exports.js  => generated files that facilitate interacting with your Amplify API from your frontend application.
+#src/graphql/**/*.ts
 amplify push 
 #eidt schema and update
 amplify push  --allow-destructive-graphql-schema-updates
 
-#this will genearte the 'src/graphql' folder + aws-exports.js  => generated files that facilitate interacting with your Amplify API from your frontend application.
 
 # open your AWS account amolify console in the web browser
 amplify console api
+
+
+# regenerate using typescript
+amplify remove api
+amplify push
