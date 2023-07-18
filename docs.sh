14844a75538d2eb97f2a316f34a7dffc0d4a7ba2
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-07-18 12:05:29 +0800

    amplify add api and push

--- docs.sh
+++ docs.sh
@@ -7,3 +7,10 @@ amplify init
 
 #amplify + react
 npm i @aws-amplify/ui-react aws-amplify
+
+#add a new GraphQL API to your Amplify project.
+#by default he API will be configured to use DynamoDB as the data source. 
+amplify add api
+
+# deploy your local backend resources defined in the Amplify project to the cloud.
+amplify push 
