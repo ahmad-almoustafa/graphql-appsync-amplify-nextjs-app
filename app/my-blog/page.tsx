2e472b1c02fc6a87f2a29296c14a8778ed2eddbd
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-07-20 11:33:29 +0800

    Frontend: Nextjs basic layout and routing

--- app/my-blog/page.tsx
+++ app/my-blog/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 import PostList from '@/components/blog/PostList';
 import {Post} from '@/src/API';
+import { deletePost } from '@/src/graphql/mutations';
 import { postByUsername } from '@/src/graphql/queries';
-import { API, Auth } from 'aws-amplify';
-import { Metadata, ResolvingMetadata } from 'next';
+import { getCurrentAuthenticatedUser } from '@/utils/helpers';
+import { API } from 'aws-amplify';
 import { useEffect, useState } from 'react';
+
   
 export default  function MyBlog({ params }: { params: { id: string } }){
     const [myposts, setMyPosts] = useState<Post[]>([]);
-    useEffect(() => {
-      const fetchMyPosts = async () => {
+    const fetchMyPosts = async () => {
         try {
             // authMode => default authorization mode is API key which configured for the app  @/configureAmplify;
-            const {username} = await Auth.currentAuthenticatedUser({
-                bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-              });
-              console.log("username", username);
+            const {username}=  await getCurrentAuthenticatedUser()
             const response = (await API.graphql({ 
                 query: postByUsername, 
                 variables: { username},
@@ -24,21 +22,58 @@ export default  function MyBlog({ params }: { params: { id: string } }){
             } )) as  { data: { postByUsername: { items: Post[] } } };
             console.log("response", response);
             setMyPosts(response.data.postByUsername.items);
-          } catch (error) {
-            console.log("Error fetching posts:", error);
-          }
-      };
-  
+        } catch (error) {
+            console.log("Error fetching user posts:", error);
+        }
+    };
+
+    useEffect(() => {
       fetchMyPosts();
     }, []);
- 
 
+    const deleteUserPost = async (id:string) => {
+        try {
+            const {username}=  await getCurrentAuthenticatedUser()
+            const response = (await API.graphql({ 
+                query: deletePost, 
+                variables: {
+                    input: { id }, // Provide the input object with the 'id' property
+                },
+                authMode: "AMAZON_COGNITO_USER_POOLS"
+            
+            } )) as  { data: { deletePost:  Post } };
+            // console.log("response", response.data.deletePost);
+            // refetch the posts => update myposts state => trigger rerendering
+            fetchMyPosts();
+           
+        } catch (error) {
+            console.log("Error deleting post:", error);
+        }
+    }
+    // JSX component name must start with a capital letter. 
+    const Actions:React.FC<Post> =(post:Post)=>{
+        return(
+            <div className="flex justify-end mt-4 space-x-2">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+              Edit
+            </button>
+            <button 
+                className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
+                onClick={() => deleteUserPost(post.id)}
+          
+            >
+              Delete
+              
+            </button>
+          </div>
+        )
+    }
     return (
         <div className="bg-gray-100 min-h-screen">
           <header className="text-center pt-5  px-6">
             <h1 className="text-2xl py-5 font-bold">My Blog Page!</h1>
           </header>
-          <PostList posts={myposts}/>
+          <PostList posts={myposts} Actions={Actions}/>
         </div>
       );
       
--- components/blog/PostList.tsx
+++ components/blog/PostList.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 
 interface PostListProps {
     posts: Post[];
+    Actions?:React.FC<Post> ;
   };
 
-export default function PostList({posts}:PostListProps)  {
+export default function PostList({posts,Actions}:PostListProps)  {
   return (
     <main className="container mx-auto py-8">
       <div className="grid grid-cols-2 gap-4">
@@ -15,6 +16,8 @@ export default function PostList({posts}:PostListProps)  {
               <h2 className="text-xl font-bold mb-2 text-blue-500 hover:text-blue-600">{post.title}</h2>
             </Link>
             <p className="text-gray-600">Author: {post.owner || "Anonymous"}</p>
+            {Actions && Actions(post)} {/* Render the actions component */}
+
           </div>
         ))}
       </div>
